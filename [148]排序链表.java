6c7eac0035f0a66b92ecd9542ca9c31656eb408d
codersimon6 <2535342895@qq.com>
2020-11-23 08:23:00 +0800

    2020-11-23

---
+++ [148]排序链表.java
@@ -0,0 +1,79 @@
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
+//
+// 进阶： 
+//
+// 
+// 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 104] 内 
+// -105 <= Node.val <= 105 
+// 
+// Related Topics 排序 链表 
+// 👍 832 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        ListNode newhead = new ListNode(0);
+        ListNode pre ;
+        newhead.next = head;
+
+        while(head != null && head.next != null) {
+            if(head.val <= head.next.val) {
+                head = head.next;
+                continue;
+            }
+            pre = newhead;
+
+            while (pre.next.val < head.next.val) pre = pre.next;
+
+            ListNode curr = head.next;
+            head.next = curr.next;
+            curr.next = pre.next;
+            pre.next = curr;
+        }
+        return newhead.next;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
