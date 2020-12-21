4c6c3b66cfa4f3539c4d598b5f86b192db097a26
codersimon6 <2535342895@qq.com>
2020-12-21 08:53:53 +0800

    2020-12-21

---
+++ [48]旋转图像.java
@@ -0,0 +1,72 @@
+//给定一个 n × n 的二维矩阵表示一个图像。 
+//
+// 将图像顺时针旋转 90 度。 
+//
+// 说明： 
+//
+// 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。 
+//
+// 示例 1: 
+//
+// 给定 matrix = 
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+// 
+//
+// 示例 2: 
+//
+// 给定 matrix =
+//[
+//  [ 5, 1, 9,11],
+//  [ 2, 4, 8,10],
+//  [13, 3, 6, 7],
+//  [15,14,12,16]
+//], 
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [15,13, 2, 5],
+//  [14, 3, 4, 1],
+//  [12, 6, 8, 9],
+//  [16, 7,10,11]
+//]
+// 
+// Related Topics 数组 
+// 👍 672 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 先以对角线（左上-右下）为轴进行翻转
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // 再对每一行以中点进行翻转
+        int mid = n >> 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < mid; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
