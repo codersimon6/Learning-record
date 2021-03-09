1881d0d91061b6e0a7d4ab11be5f866f8d8f6a68
codersimon6 <2535342895@qq.com>
2021-03-09 09:03:18 +0800

    1

--- [21]合并两个有序链表.java
+++ [21]合并两个有序链表.java
@@ -1,17 +1,3 @@
-//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
-//
-// 
-//
-// 示例： 
-//
-// 输入：1->2->4, 1->3->4
-//输出：1->1->2->3->4->4
-// 
-// Related Topics 链表 
-// 👍 1276 👎 0
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -24,7 +10,22 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode cur = new ListNode();
+        ListNode head = cur;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 != null)cur.next = l1;
+        if(l2 != null)cur.next = l2;
+        return head.next;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
